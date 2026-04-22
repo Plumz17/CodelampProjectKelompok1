bd6e57bc4c6c581696c5b69223b5688c8d986a75
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-04-22 22:51:47 +0700

    Revert "Merged"
    This reverts commit 0f4ab0839b905a024d0962ec7a858a05aaa7c460, reversing
    changes made to 0eacd0485e9b177c838f9c54dd631f4e9270beef.

--- CodelampProject/ghosts/kunilanak.gd
+++
@@ -1,8 +0,0 @@
-extends GhostBase
-class_name Kuntilanak
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	#Test the inheritence (Remove later)
-	super._ready()
-	print("%s, %s, %s, %s, %s" % [fear_damage, attack_rate, cost, cost_upgrade, cost_move])
