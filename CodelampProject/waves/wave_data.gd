c00647012b724a01e5b46096a88330785946bc08
Hazard-Nico <nugrahaji98@gmail.com>
2026-04-28 19:38:24 +0700

    fix start wave dan worked level scene

--- CodelampProject/waves/wave_data.gd
+++ CodelampProject/waves/wave_data.gd
@@ -1,5 +1,6 @@
 extends Resource
 class_name WaveData
 
-@export var enemies: Array[EnemyEntry] = []
+@export var enemy_scenes: Array[PackedScene] = []
+@export var enemy_counts: Array[int] = []
 @export var interval: float = 1.5
