0ab696c74e16734ae46617a5f4066fc24b102059
Plumz17 <121227766+Plumz17@users.noreply.github.com>
2026-05-04 15:42:42 +0700

    Add Level Select Menu

--- CodelampProject/globals/signal_hub.gd
+++ CodelampProject/globals/signal_hub.gd
@@ -2,9 +2,17 @@ extends Node
 
 signal show_settings
 signal hide_settings
+signal show_level_select
+signal hide_level_select
 
 func emit_show_settings() -> void:
 	show_settings.emit()
 
 func emit_hide_settings() -> void:
 	hide_settings.emit()
+
+func emit_show_level_select() -> void:
+	show_level_select.emit()
+
+func emit_hide_level_select() -> void:
+	hide_level_select.emit()
